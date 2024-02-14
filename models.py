6310bc9c60dce7992060be0709288b8de3165d37
slidebrass <slidebrass@gmail.com>
2024-02-14 10:18:57 -0600

    db init

--- models.py
+++ models.py
@@ -45,7 +45,7 @@ class BreedInfo(db.Model):
     user_token = db.Column(db.String, db.ForeignKey('user.token'), nullable=False)
 
     def __init__(self, breed_id, breed_name, breed_group, life_span, weight, height, bred_for, temperament, reference_image_id, user_token):
-        self.breed_id = breed_id
+        self.breed_id = self.set_id()
         self.breed_name = breed_name
         self.breed_group = breed_group
         self.life_span = life_span
@@ -55,6 +55,9 @@ class BreedInfo(db.Model):
         self.temperament = temperament
         self.reference_image_id = reference_image_id
         self.user_token = user_token
+
+    def set_id(self):
+        return str(uuid.uuid4())
     
     def __repr__(self):
         return f'The {self.breed_name} breed has been added to your favorites.'
@@ -90,38 +93,3 @@ class BreedNotesSchema(ma.Schema):
 
 breed_notes_schema = BreedNotesSchema()
 breeds_notes_schema = BreedNotesSchema(many=True)
-
-# creating a class to store breed information gathered from the Dog API
-class BreedInfo(db.Model):
-    breed_id = db.Column(db.Integer, primary_key=True)
-    breed_name = db.Column(db.String(100))
-    breed_group = db.Column(db.String(100))
-    life_span = db.Column(db.String(100))
-    weight = db.Column(db.String(100))
-    height = db.Column(db.String(100))
-    bred_for = db.Column(db.String(200))
-    temperament = db.Column(db.String(500))
-    reference_image_id = db.Column(db.String(50))
-    user_token = db.Column(db.String, db.ForeignKey('user.token'), nullable=False)
-
-    def __init__(self, breed_id, breed_name, breed_group, life_span, weight, height, bred_for, temperament, reference_image_id, user_token):
-        self.breed_id = breed_id
-        self.breed_name = breed_name
-        self.breed_group = breed_group
-        self.life_span = life_span
-        self.weight = weight
-        self.height = height
-        self.bred_for = bred_for
-        self.temperament = temperament
-        self.reference_image_id = reference_image_id
-        self.user_token = user_token
-    
-    def __repr__(self):
-        return f'The {self.breed_name} breed has been added to your favorites.'
-    
-class BreedInfoSchema(ma.Schema):
-    class Meta:
-        fields = ['breed_id', 'breed_name', 'breed_group', 'life_span', 'weight', 'height', 'bred_for', 'temperament', 'reference_image_id']
-
-breed_info_schema = BreedInfoSchema()
-breeds_info_schema = BreedInfoSchema(many=True)
